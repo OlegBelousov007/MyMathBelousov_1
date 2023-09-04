e2ab07926b4464692d14786139facad951bb848f
OlegBelousov007 <belou5offoleg@yandex.ru>
2023-09-04 16:47:27 +0300

    My math

---
+++ src/main/java/org/example/MyMath.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class MyMath {
+    public static final double E = 2.718281828459045;
+    public static final double PI = 3.141592653589793;
+
+    static double min(double firstValue, double secondValue) {
+        if (firstValue < secondValue) {
+            return firstValue;
+        } else {
+            return secondValue;
+        }
+    }
+
+    public static void arcSin(double angleArcSin) {
+
+    }
+}
